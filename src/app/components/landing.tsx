bebf3b42f792547cec0763fe5fa4414ac38e8477
Leroy Lau <llau65@my.bcit.ca>
2023-09-17 22:56:06 -0700

    Added descriptions and cards to projects section, and changed layout for landing

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -5,11 +5,15 @@ import styles from "../css/landing.module.css";
 
 export default function Landing() {
   return (
-    <section className={styles.hero} id="landing" data-section>
+    <section className={styles.landing} id="landing" data-section>
       <div className={styles.socials}>
         <ul>
           <li>
-            <Link href="https://github.com/LeroyL711" target="blank" className={styles.socialLinks}>
+            <Link
+              href="https://github.com/LeroyL711"
+              target="blank"
+              className={styles.socialLinks}
+            >
               <Image
                 src="https://d33wubrfki0l68.cloudfront.net/38469cf88d038b6ba3322c9fcb93a8f7167df4b9/cb0b9/assets/svg/github-dark.svg"
                 alt="Link to Leroy's GitHub"
@@ -19,43 +23,27 @@ export default function Landing() {
             </Link>
           </li>
           <li>
-            <Link href="https://www.linkedin.com/in/leroy-lau/" target="blank" className={styles.socialLinks}>
-              <Image 
-              src="https://d33wubrfki0l68.cloudfront.net/d8e6e1e636531e28274a1b8b6d947b817f6145bd/d42d3/assets/svg/linkedin-dark.svg"
-              alt="Link to Leroy's LinkedIn"
-              width={30}
-              height={30}
-              />
-            </Link>
-          </li>
-          <li>
-            <Link href="https://discord.com/users/268973793425358868" className={styles.socialLinks}>
+            <Link
+              href="https://www.linkedin.com/in/leroy-lau/"
+              target="blank"
+              className={styles.socialLinks}
+            >
               <Image
-              src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6918e57475a843f59f_icon_clyde_black_RGB.svg"
-              alt="Link to Leroy's Discord"
-              width={30}
-              height={30}/>
+                src="https://d33wubrfki0l68.cloudfront.net/d8e6e1e636531e28274a1b8b6d947b817f6145bd/d42d3/assets/svg/linkedin-dark.svg"
+                alt="Link to Leroy's LinkedIn"
+                width={30}
+                height={30}
+              />
             </Link>
           </li>
         </ul>
       </div>
-      <div className={styles.contentContainer}>
-        <div className={styles.heroContent}>
-          <div className={styles.heroHeading}>
-            Hey, I'm <span className={styles.name}>Leroy Lau</span>
-          </div>
-          <div className={styles.heroText}>
-            Welcome to my site! Here, you will be able to see some of the things
-            I've worked on, as well as more about myself!
-          </div>
+      <div className={styles.mainContainer}>
+        <div className={styles.landingHeading}>
+          Hey, I'm <span className={styles.name}>Leroy Lau<span className={styles.period}>.</span></span>
         </div>
-        <div className={styles.heroImageContainer}>
-          <Image
-            className={styles.heroImage}
-            src={laptopPic}
-            fill
-            alt="Colourful picture of a laptop"
-          />
+        <div className={styles.landingText}>
+        A self-motivated, collaborative, and lifelong learner with a passion for problem-solving in the ever-evolving world of web development.
         </div>
       </div>
     </section>
--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,38 +1,54 @@
 import styles from "../css/projects.module.css";
+import laptopPic from "../images/laptop.jpg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Projects() {
   return (
     <section className={styles.projects} id="projects" data-section>
       <div className={styles.mainContainer}>
         <div className={styles.projectsHeader}>Projects</div>
-        <div className={styles.projectsText}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          recusandae natus a labore nulla nobis perspiciatis magni maxime harum
-          fugit culpa quidem, sint id! Mollitia non, maiores adipisci nostrum
-          cupiditate harum alias minima eum numquam, asperiores aliquid animi.
-          Magni magnam omnis aperiam assumenda cupiditate, dignissimos nihil et!
-          Veniam, quasi animi.
-        </div>
 
         <div className={styles.projectsContainer}>
           <div className={styles.project}>
             <h3 className={styles.projectName}>CampPass</h3>
             <p className={styles.projectDescription}>
-              CampPass is application that enables users to check the availability of daypasses for provincial parks on the BC Parks Website.
+              CampPass is application that enables users to check the
+              availability of daypasses for provincial parks on the BC Parks
+              Website.
             </p>
+            <div className={styles.imageContainer}>
+              <Image
+                className={styles.projectImage}
+                style={{objectFit:"cover"}}
+                src={laptopPic}
+                fill
+                alt="Colourful picture of a laptop"
+              />
+            </div>
             <ul className={styles.techStack}>
               <li className={styles.tech}>Express.js</li>
               <li className={styles.tech}>React</li>
               <li className={styles.tech}>Node.js</li>
               <li className={styles.tech}>Python</li>
-              <li className={styles.tech}>Selenium</li>  
+              <li className={styles.tech}>Selenium</li>
             </ul>
           </div>
           <div className={styles.project}>
             <h3 className={styles.projectName}>Healthify</h3>
             <p className={styles.projectDescription}>
-              Healthify is a fitness and nutrition application that leverages AI to generate personalized workout and meal plans for users.
+              Healthify is a fitness and nutrition application that leverages AI
+              to generate personalized workout and meal plans for users.
             </p>
+            <div className={styles.imageContainer}>
+              <Image
+                className={styles.projectImage}
+                src={laptopPic}
+                style={{objectFit:"cover"}}
+                fill
+                alt="Colourful picture of a laptop"
+              />
+            </div>
             <ul className={styles.techStack}>
               <li className={styles.tech}>MongoDB</li>
               <li className={styles.tech}>Express.js</li>
@@ -44,7 +60,20 @@ export default function Projects() {
 
           <div className={styles.project}>
             <h3 className={styles.projectName}>ReQuest</h3>
-            <p className={styles.projectDescription}>ReQuest is a community-oriented application where users can create posts requesting help from their neighbours, or offer help to anyone that is in need.</p>
+            <p className={styles.projectDescription}>
+              ReQuest is a community-oriented application where users can create
+              posts requesting help from their neighbours, or offer help to
+              anyone that is in need.
+            </p>
+            <div className={styles.imageContainer}>
+              <Image
+                className={styles.projectImage}
+                src={laptopPic}
+                fill
+                style={{objectFit:"cover"}}
+                alt="Colourful picture of a laptop"
+              />
+            </div>
             <ul className={styles.techStack}>
               <li className={styles.tech}>HTML</li>
               <li className={styles.tech}>CSS</li>
